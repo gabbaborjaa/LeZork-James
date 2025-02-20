8094aa1a20e6a63b991376e731fe13ad32977ee6
Drew Baine <bainedrew@gmail.com>
2025-02-20 13:43:40 -0500

    Location is working. Uses reference wrappers for map

---
+++ Location.hpp
@@ -0,0 +1,81 @@
+#ifndef __HPP__LOCATION__
+#define __HPP__LOCATION__
+
+#include <vector>
+#include <iostream>
+#include <map>
+#include <string>
+#include <stdexcept>
+#include <functional>
+#include "npc.hpp"
+#include "item.hpp"
+
+class Location{
+
+    private:
+        std::string name;
+        std::string description;
+        bool visited = false;
+        std::map<std::string, std::reference_wrapper<Location>> neighbors; 
+        std::vector<NPC> NPCs;
+        std::vector<Item> items;
+
+    public:
+        Location(){
+            this->name = "";
+            this->description = "";
+       }
+        Location(std::string name, std::string description){
+            this->name = name;
+            this->description = description;
+        }
+
+
+        friend std::ostream& operator<<(std::ostream& os, const Location& obj){
+        os << obj.name <<"\n\t" << obj.description;
+        return os;
+        }
+       // void add_location(std::string direction, std::reference_wrapper<Location> location) {
+        void add_location(std::string direction,Location& location) {
+            //used chatgpt for exception handling
+        if (direction.empty()) {
+            throw std::invalid_argument("Direction cannot be blank.");
+        }
+        if (neighbors.find(direction) != neighbors.end()) {
+            throw std::invalid_argument("Key already exists in the map.");
+        }
+//        this->neighbors.insert(std::pair<std::string, std::reference_wrapper<Location>>(direction, std::ref(location)));
+           //used CHATGPT    
+            this->neighbors.emplace(std::piecewise_construct,
+                       std::forward_as_tuple(direction),
+                       std::forward_as_tuple(std::ref(location))); 
+    }
+        const std::map<std::string, std::reference_wrapper<Location>>& get_locations() const {
+        return this->neighbors; 
+        }
+        void set_visited(){
+            this->visited = true;
+        }
+        bool get_visited(){
+            if(visited){
+                return true;
+            }
+            return false;
+         }
+        void add_npc(NPC& npc){
+            this->NPCs.push_back(npc);
+        }
+
+        void add_item(Item& item){
+            this->items.push_back(item);
+        }
+        std::vector<Item>& get_items(){
+            return this->items;
+        }
+
+        std::vector<NPC>& get_NPCs(){
+            return this->NPCs;
+        }
+ };
+
+ #endif

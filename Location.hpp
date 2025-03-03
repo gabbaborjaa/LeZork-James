22a1d5b98a0af6272f85c22eb8bb50d288c26f03
drewbaine <139158590+drewbaine@users.noreply.github.com>
2025-03-03 00:31:34 -0500

    Added Documentation to Location.hpp

--- Location.hpp
+++ Location.hpp
@@ -30,6 +30,7 @@ class Location{
 
     public:
         Location(){
+            //blank constructor
             this->name = "";
             this->description = "";
         }
@@ -39,11 +40,13 @@ class Location{
         }
 
         friend std::ostream& operator<<(std::ostream& os, const Location& obj){
+            //printing
             os << obj.name << " - " << obj.description;
             return os;
         }
         // void add_location(std::string direction, std::reference_wrapper<Location> location) {
         void add_location(std::string direction,Location& location) {
+            //adds location to neighbors. converts to reference wrapper of location first
             //used chatgpt for exception handling
             if (direction.empty()) {
                 throw std::invalid_argument("Direction cannot be blank.");
@@ -58,38 +61,47 @@ class Location{
                     std::forward_as_tuple(std::ref(location)));
         }
         const std::map<std::string, std::reference_wrapper<Location> >& get_locations() const {
+            //returns map of locations
             return this->neighbors;
         }
         void set_visited(){
+            //sets visited to true. cannot be changed to false
             this->visited = true;
         }
         bool get_visited(){
+            //returns if the locaiton is visited
             if(visited){
                 return true;
             }
             return false;
          }
         void add_npc(NPC& npc){
+            //adds an npc to list of npcs
             this->NPCs.push_back(npc);
         }
 
         void add_item(Item& item){
+            //adds an item
             this->items.push_back(item);
         }
 
         void remove_item(Item& item){
+            //removes an item
             this->items.erase(std::remove(this->items.begin(), this->items.end(), item));
         }
 
         std::vector<Item>& get_items(){
+            //returns vector of items
             return this->items;
         }
 
         std::vector<NPC>& get_NPCs(){
+            //returns vector of NPCs
             return this->NPCs;
         }
 
         std::string get_name(){
+            //returns vector of names
             return this->name;
         }
 
